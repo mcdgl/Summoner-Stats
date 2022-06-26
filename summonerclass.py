f95d94bccce656c0b29b02c72ca1d5428db5af2f
mcdgl <mcdangel@asu.edu>
2022-06-26 00:01:07 -0700

    Added handling for users that do not exist

--- summonerclass.py
+++ summonerclass.py
@@ -6,10 +6,17 @@ from selenium import webdriver
 from bs4 import BeautifulSoup #webscraping library
 
 
-#page = requests.get()
+
 
 class Summoner:
     def __init__(self, name = "", region = ""):
         self.name = name
         self.region = region
         self.opgg = "https://" + region.lower() + ".op.gg/summoners/" + region.lower() + "/" + name.replace(" ", "%2B")
+        page = requests.get(opgg, headers={'User-Agent': 'Mozilla/5.0'}).text
+        soup = BeautifulSoup(page, "html.parser")
+
+        #searches to see if the page has the element saying that a user does not exist
+        exists = soup.find('h2').text
+        if("This summoner is not" in exists):
+            raise Exception("HELP I AM TRAPPED IN A PYTHON SCRIPT")
