de85d8d19f86b13fdf27b783ef2417539e1c422c
mcdgl <mcdangel@asu.edu>
2022-06-25 19:33:51 -0700

    Added framework for opgg command

--- main.py
+++ main.py
@@ -20,11 +20,21 @@ async def on_message(message):
     #processing commands
     await client.process_commands(message)
 
+#test command
 @client.command()
 async def hi(ctx):
     print('lebron james!')
     await ctx.send('Hello!')
 
-    
+@client.command()
+async def opgg(ctx, region=None, sumName = None):
+    if(region ==None or sumName == Nome):
+        await ctx.send(f'Invalid input; Enter as follows: "!op.gg [region] [summoner name]" and try again')
+    elif(len(region)>3):
+        await ctx.send(f'Invalid input; Make sure you are typing the region abbrevation (as follows: NA = North America, KR = Korea, etc)')
+    else:
+        await ctx.send(f'Region: {region.upper()}')
+        await ctx.send(f'Summoner Name: {sumName}')
+
 #runs client
 client.run(my_secret)
