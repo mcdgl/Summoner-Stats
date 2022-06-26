9267de5d76676a246a75345820742225fcce4c18
mcdgl <mcdangel@asu.edu>
2022-06-25 17:15:44 -0700

    Created template based on old files (merging)

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#testing git
+import discord
+import os
+from discord.ext import commands
+
+client = discord.Client()
+cmd_client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print("We have logged in as  {0.user}".format(client))
+
+@cmd_client.command()
