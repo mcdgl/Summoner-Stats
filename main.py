ad9e6a54c224ce9ed4a217cac499fdc52728fd04
Nicodemus Phaklides <nphaklides@hotmail.com>
2022-06-25 18:14:48 -0700

    Bot process commands

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("We have logged in as  {0.user}".format(client))
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    await client.process_commands(message)
+
 
 client.run(my_secret)
