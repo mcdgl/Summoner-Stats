a8f329337f78569d771e665c3251422bb48ddcff
mcdgl <mcdangel@asu.edu>
2022-06-25 18:22:22 -0700

    test again

--- botcommands.py
+++ botcommands.py
@@ -1,9 +1,12 @@
 import discord
 from discord.ext import commands
 
+my_secret = os.environ['TOKEN']
 bot = commands.Bot(command_prefix = "!")
 
 @bot.command()
 async def hi(ctx):
     print('lebron james!')
     await ctx.send('Hello!')
+
+bot.run(my_secret)
--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import os
 import botcommands
 from discord.ext import commands
 
+#discord api key stored in env variables for security (this is a public repo)
 my_secret = os.environ['TOKEN']
 client = discord.Client()
 
-
+#debug event to see if bot has logged on to discord
 @client.event
 async def on_ready():
     print("We have logged in as  {0.user}".format(client))
+
+#event handler for messages sent in discord server
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    #processing commands
     await client.process_commands(message)
 
-
+#runs client
 client.run(my_secret)
